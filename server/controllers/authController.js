26556e633e35823b6227c9b930d2c1b48a5cb3fd
Meenakshi-Jisha <jisha20022@gmail.com>
2026-05-21 06:04:32 +0530

    Added User Profile view API

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -121,6 +121,35 @@ const uploadResume=async(req,res)=>{
     }
 
 }
+const getProfile = async (req, res) => {
+
+    try {
+
+        const user = await User.findById(
+        req.user.id
+        ).select("-password");
+
+        if (!user) {
+
+        return res.status(404).json({
+            message: "User not found"
+        });
+
+        }
+
+        res.status(200).json(user);
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+        message: "Server Error"
+        });
+
+    }
+
+};
 const updateProfile =async(req,res)=>{
     try{
             const user=await User.findById(req.user.id);
@@ -166,4 +195,4 @@ const updateProfile =async(req,res)=>{
 
     }
 
-module.exports = {registerUser,loginUser,uploadResume,updateProfile};
+module.exports = {registerUser,loginUser,getProfile,uploadResume,updateProfile};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { registerUser ,loginUser,updateProfile,uploadResume} = require("../controllers/authController");
+const { registerUser ,loginUser,getProfile,updateProfile,uploadResume} = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
 const authorizeRoles = require("../middleware/roleMiddleware");
 const upload =require("../middleware/uploadMiddleware");
@@ -11,18 +11,19 @@ router.post("/register", registerUser);
 // login route
 router.post("/login", loginUser);
 
-router.get("/profile", authMiddleware, (req, res) => {
-    res.status(200).json({
-        message: "Welcome to profile",
-        user: req.user,
-    });
-});
+// router.get("/profile", authMiddleware, (req, res) => {
+//     res.status(200).json({
+//         message: "Welcome to profile",
+//         user: req.user,
+//     });
+// });
 router.get("/recruiter-dashboard",authMiddleware,authorizeRoles("recruiter"),(req, res) => {
         res.json({
         message: "Welcome Recruiter",
         });
     }
 );
+router.get("/profile",authMiddleware,getProfile);
 router.put("/profile",authMiddleware,updateProfile);
 router.post("/upload-resume",authMiddleware,upload.single("resume"),uploadResume);
 module.exports = router;
