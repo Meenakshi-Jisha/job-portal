805a982ec7dcbaded6fd33728ec3cdcdda3d4553
Meenakshi-Jisha <jisha20022@gmail.com>
2026-05-22 06:42:48 +0530

    Added remove bookmark API

--- server/controllers/bookmarkController.js
+++ server/controllers/bookmarkController.js
@@ -3,22 +3,18 @@ require("../models/Bookmark");
 
 const saveJob =async(req,res)=>{
     try{
-
         const {jobId}=req.params;
-
         const exists=await Bookmark.findOne({
             user:req.user.id,
             job:jobId
         });
 
         if(exists){
-
         return res.status(400).json({
             message:
             "Already saved"
         });
         }
-
         const bookmark=await Bookmark.create({
             user:req.user.id,
             job:jobId
@@ -27,41 +23,61 @@ const saveJob =async(req,res)=>{
         res.status(201).json({
             message:
             "Job saved",
-
             bookmark
-
         });
-
     }catch(error){
         console.log(error);
         res.status(500).json({
         message:"Server Error"
         });
-
     }
-
 };
 const getSavedJobs =async(req,res)=>{
     try{
         const bookmarks=await Bookmark.find({
             user:req.user.id
-
         }).populate("job");
 
         res.status(200).json({
-
         count:
         bookmarks.length,
-
         bookmarks
-
         });
 
     }catch(error){
-
         res.status(500).json({
         message:"Server Error"
         });
     }
 }
-module.exports={saveJob,getSavedJobs}
+const removeBookmark =async(req,res)=>{
+    try{
+        const {jobId}=req.params;
+        const bookmark=await Bookmark.findOne({
+            user:req.user.id,
+            job:jobId
+        });
+
+        if(!bookmark){
+            return res.status(404).json({
+                message:
+                "Bookmark not found"
+            });
+        }
+        await Bookmark.findByIdAndDelete(bookmark._id);
+        res.status(200)
+        .json({
+            message:
+            "Bookmark removed"
+        });
+
+    }catch(error){
+        console.log(error);
+        res.status(500)
+        .json({
+            message:
+            "Server Error"
+        });
+        }
+};
+module.exports={saveJob,getSavedJobs,removeBookmark}
--- server/routes/bookmarkRoutes.js
+++ server/routes/bookmarkRoutes.js
@@ -1,12 +1,12 @@
 const express =require("express");
 
 const router=express.Router();
-const { saveJob,getSavedJobs} = require("../controllers/bookmarkController");
+const { saveJob,getSavedJobs,removeBookmark} = require("../controllers/bookmarkController");
 const authMiddleware = require("../middleware/authMiddleware");
 const authorizeRoles = require("../middleware/roleMiddleware");
 
 
 router.post("/:jobId",authMiddleware,authorizeRoles("jobseeker"),saveJob);
 router.get("/",authMiddleware,authorizeRoles("jobseeker"),getSavedJobs);
-
+router.delete("/:jobId",authMiddleware,authorizeRoles("jobseeker"),removeBookmark);
 module.exports=router;
