6ee92968b10c725f6d9e9f1c62d9c7020bcfa308
Meenakshi-Jisha <jisha20022@gmail.com>
2026-05-28 18:48:38 +0530

    Connected profile page API

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { getProfile } from '../services/authService'
 
 function Profile() {
+  const [user,setUser]=useState(null)
+  useEffect(()=>{
+    fetchProfile()
+  },[])
+  const fetchProfile=async()=>{
+    try{
+      const res= await getProfile();
+      console.log(res.data);
+      
+      setUser(res.data)
+    }
+    catch(error){
+      console.log(error);
+      
+    }
+  }
+  if(!user){
+    return(
+      <h2>Loading...</h2>
+    )
+  }
   return (
-    <div>Profile</div>
+    <div>
+      <h1>Profile</h1>
+      <h3>Name:{user.name}</h3>
+      <p>Email:{user.email}</p>
+      <p>Role:{user.role}</p>
+      <p>Bio:{user.bio}</p>
+      <p>Phone:{user.phone}</p>
+    </div>
   )
 }
 
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,15 +1,29 @@
 import axios from "axios";
-
 const API = "http://localhost:5000/api/auth";
+
 export const loginUser= (data)=>{
     return axios.post(
         `${API}/login`,
         data
     )
 }
+
 export const registerUser= (data)=>{
     return axios.post(
         `${API}/register`,
         data
     )
+}
+
+export const getProfile=()=>{
+const token=localStorage.getItem("token");
+    return axios.get(
+        `${API}/profile`,
+        {
+            headers:{
+                Authorization:
+                `Bearer ${token}`
+            }
+        }
+    );
 }
