e07f284a40e8b42428fe4c9f5f6b1c62fa5245f8
Meenakshi-Jisha <jisha20022@gmail.com>
2026-06-02 12:42:08 +0530

    Created home page ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {Link} from "react-router-dom";
 
-function Home() {
-  return (
-    <div>Home</div>
+function Home(){
+  return(
+    <div>
+      <h1>  Find Your Dream Job </h1>
+      <p>Search jobs,apply easily,and get hired.</p><br/>
+      <Link to="/jobs"><button> Browse Jobs</button></Link>{" "}
+      <Link to="/register"><button> Get Started</button></Link>
+      <hr/>
+      <h2> Why Choose Us? </h2>
+      <ul>
+        <li>Apply for jobs easily </li>
+        <li> Recruiters can post jobs </li>
+        <li> Track application status </li>
+        <li> Build professional profile </li>
+      </ul>
+    </div>
   )
 }
-
-export default Home
+export default Home;
